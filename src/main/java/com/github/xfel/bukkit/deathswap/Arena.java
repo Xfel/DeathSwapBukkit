0539d64576b5b8484a85dc637174ef49afe48f39
Xfel <su30vaju@clientmaster64.rbg.informatik.tu-darmstadt.de>
2013-05-22 12:13:15 +0200

    Interface definition begin

---
+++ src/main/java/com/github/xfel/bukkit/deathswap/Arena.java
@@ -0,0 +1,14 @@
+package com.github.xfel.bukkit.deathswap;
+
+public class Arena {
+	
+	public static void swapPlayers(Player p1, Player p2) {
+		
+	}
+	
+	private Player p1;
+	
+	private Player p2;
+	
+	
+}
