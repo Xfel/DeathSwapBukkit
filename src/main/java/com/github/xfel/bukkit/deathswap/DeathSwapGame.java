5cb28bf9e7455a6911431fdea723d40d6114822c
Xfel <su30vaju@clientmaster64.rbg.informatik.tu-darmstadt.de>
2013-05-22 12:06:35 +0200

    Basic structure generated

---
+++ src/main/java/com/github/xfel/bukkit/deathswap/DeathSwapGame.java
@@ -0,0 +1,17 @@
+package com.github.xfel.bukkit.deathswap;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DeathSwapGame extends JavaPlugin {
+
+	@Override
+	public void onEnable(){
+		// TODO Insert logic to be performed when the plugin is enabled
+	}
+	
+	@Override
+	public void onDisable() {
+		// TODO Insert logic to be performed when the plugin is disabled
+	}
+	
+}
