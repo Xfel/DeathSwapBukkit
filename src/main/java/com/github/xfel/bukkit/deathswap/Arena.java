a269a6fa2a84e320ca1aebc38671080605422a77
Xfel <felixtreede@yahoo.de>
2013-05-24 09:48:52 +0200

    improved plugin.yml, configured .gitignore for eclipse

--- src/main/java/com/github/xfel/bukkit/deathswap/Arena.java
+++ src/main/java/com/github/xfel/bukkit/deathswap/Arena.java
@@ -1,9 +1,25 @@
 package com.github.xfel.bukkit.deathswap;
 
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+
 public class Arena {
 	
+	/**
+	 * Will swap the two player's location.
+	 * 
+	 * @param p1
+	 * @param p2
+	 */
 	public static void swapPlayers(Player p1, Player p2) {
 		
+		Location p1loc = p1.getLocation();
+		
+		Location p2loc = p2.getLocation();
+		
+		
+		p1.teleport(p2loc);
+		p2.teleport(p1loc);
 	}
 	
 	private Player p1;
